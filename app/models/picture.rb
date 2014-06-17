98b4200579b2fcb90123dee7c20ce170d41f34c7
jonssmall <jonssmall@gmail.com>
2014-06-17 16:18:47 -0400

    I created a Picture model(?)

---
+++ app/models/picture.rb
@@ -0,0 +1,11 @@
+class Picture < ActiveRecord::Base
+  def change
+    create_table :pictures do |t|
+      t.string :artist
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
