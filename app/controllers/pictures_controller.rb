000dce95d1a9d5be366c5ca6fc9b5825132e692b
jonssmall <jonssmall@gmail.com>
2014-06-17 15:34:47 -0400

    Step #4 refactored, bring on the photos!

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,27 +19,24 @@ class PicturesController < ApplicationController
     ]
   end
 
-  def picture0
-    @picture = {
+  def show
+    @pictures = [
+      {
         :title => "The old church on the coast of White sea",
         :artist => "Sergey Ershov",
         :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
-  end
-
-  def picture1
-    @picture = {
+    },
+    {
         :title => "Sea Power",
         :artist => "Stephen Scullion",
         :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      }
-  end
-
-  def picture2
-    @picture = {
+      },
+      {
         :title => "Into the Poppies",
         :artist => "John Wilhelm",
         :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
       }
+    ]
+    @picture = @pictures[params[:id].to_i]
   end
 end
