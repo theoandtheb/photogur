e4cdffd3fc9ce225c1c1913a9c99459a2025d175
jonssmall <jonssmall@gmail.com>
2014-06-17 17:35:25 -0400

    Oh, rails.  Your resources make all that typing seem futile.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 #Rails.application.routes.draw do
   Photogur::Application.routes.draw do
-    get 'pictures' => 'pictures#index'
-    post 'pictures' => 'pictures#create'
-    get 'pictures/new' => 'pictures#new'
-    get 'pictures/:id' => 'pictures#show', as: "picture"
-    get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
-    patch 'pictures/:id' => 'pictures#update'
-    delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+    resources :pictures
+
     root :to => 'pictures#index'
 end
 
