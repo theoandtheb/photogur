5906b302259d51abe53d614ec608f0c61b6db8a1
jonssmall <jonssmall@gmail.com>
2014-06-17 16:43:06 -0400

    We now have a root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 #Rails.application.routes.draw do
   Photogur::Application.routes.draw do
+    root :to => 'pictures#index'
     get 'pictures' => 'pictures#index'
     post 'pictures' => 'pictures#create'
     get 'pictures/new' => 'pictures#new'
